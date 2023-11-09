52ed3f2a249e1952cd7d940d8ecd2d123e8edbbb
JuanLucca846 <lucca470@gmail.com>
2023-11-09 18:54:38 -0300

    add pagination

--- src/pages/biblioteca/index.tsx
+++ src/pages/biblioteca/index.tsx
@@ -10,13 +10,17 @@ import { toast } from "react-toastify";
 import { FiSearch } from "react-icons/fi";
 
 type BookProps = {
-  booksList: Array<{
-    id: number;
-    title: string;
-    author: string;
-    category: string;
-    quantity: number;
-  }>;
+  booksList: {
+    total: number;
+    totalPage: number;
+    books: Array<{
+      id: number;
+      title: string;
+      author: string;
+      category: string;
+      quantity: number;
+    }>;
+  };
 };
 
 type RentProps = {
@@ -25,16 +29,18 @@ type RentProps = {
 };
 
 export default function Library({ booksList }: BookProps) {
-  const [books, setBooks] = useState(booksList || []);
+  const [books, setBooks] = useState<BookProps["booksList"]["books"]>(booksList.books || []);
   const [search, setSearch] = useState("");
-  const [filteredBooks, setFilteredBooks] = useState(booksList || []);
+  const [filteredBooks, setFilteredBooks] = useState<BookProps["booksList"]["books"]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
 
   useEffect(() => {
     if (search) {
       const filtered = books.filter((book) => book.title.toLowerCase().includes(search.toLowerCase()));
       setFilteredBooks(filtered);
     } else {
-      setFilteredBooks(books);
+      setFilteredBooks(books || []);
     }
   }, [search, books]);
 
@@ -42,13 +48,25 @@ export default function Library({ booksList }: BookProps) {
 
   async function fetchBooks() {
     try {
-      const response = await api.get("/book");
+      const response = await api.get("/book", {
+        params: {
+          skip: Number(currentPage),
+          take: Number(itemsPerPage),
+        },
+      });
 
-      const books = response.data;
-      return books;
+      const booksData = response.data;
+
+      if (currentPage === 1) {
+        setBooks(booksData.books);
+      } else {
+        setBooks((prevBooks) => [...prevBooks, ...booksData.books]);
+      }
+
+      return booksData;
     } catch (error) {
-      toast.error("Erro");
-      console.log("Erro");
+      toast.error("Erro ao carregar os livros");
+      console.error("Erro ao carregar os livros:", error);
     }
   }
 
@@ -75,28 +93,37 @@ export default function Library({ booksList }: BookProps) {
     }
   }
 
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const changeItemsPerPage = (newItemsPerPage: number) => {
+    setItemsPerPage(newItemsPerPage);
+    setCurrentPage(1);
+  };
+
   useEffect(() => {
     const loadBooks = async () => {
       try {
         const booksData = await fetchBooks();
-        setBooks(booksData || []);
+        setBooks(booksData?.books || []);
       } catch (error) {
         console.error("Error loading books:", error);
       }
     };
 
     loadBooks();
-  }, []);
+  }, [currentPage, itemsPerPage, search]);
 
   return (
     <>
       <Head>
-        <title>NossaBiblioteca - Inicio</title>
+        <title>NossaBiblioteca - Início</title>
       </Head>
       <div>
         <Header />
         <div className={styles.availableBooks}>
-          <h2>Livros Disponiveis</h2>
+          <h2>Livros Disponíveis</h2>
           <div className={styles.searchContainer}>
             <input type="text" placeholder="Buscar livros..." value={search} onChange={(e) => setSearch(e.target.value)} className={styles.searchBook} />
             <FiSearch color="#109152" size={24} className={styles.fiSearch} />
@@ -109,7 +136,7 @@ export default function Library({ booksList }: BookProps) {
                   <p className={styles.bookAuthor}>Autor: {book.author}</p>
                   <p className={styles.bookCategory}>Categoria: {book.category}</p>
                   <p className={styles.bookQuantity}>Quantidade: {book.quantity}</p>
-                  <button type="button" className={styles.buttonBooking} onClick={() => rentBook({ userId: user.user.id || 0, bookId: book.id })}>
+                  <button type="button" className={styles.buttonBooking} onClick={() => rentBook({ userId: user.user?.id || 0, bookId: book.id })}>
                     Alugar
                   </button>
                 </div>
@@ -118,6 +145,18 @@ export default function Library({ booksList }: BookProps) {
           </ul>
         </div>
       </div>
+      <div className={styles.paginationContainer}>
+        <button type="button" className={styles.buttonPrevious} onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+          Anterior
+        </button>
+        <button type="button" className={styles.buttonNext} onClick={() => paginate(currentPage + 1)}>
+          Próxima
+        </button>
+        <select className={styles.selectionPagination} onChange={(e) => changeItemsPerPage(Number(e.target.value))} value={itemsPerPage}>
+          <option value={5}>5 por página</option>
+          <option value={10}>10 por página</option>
+        </select>
+      </div>
     </>
   );
 }
@@ -125,13 +164,21 @@ export default function Library({ booksList }: BookProps) {
 export const getServerSideProps = canSSRAuth(async (ctx) => {
   const api = setupAPIClient(ctx);
 
-  const response = await api.get("/book");
+  try {
+    const response = await api.get("/book");
+    const books = response.data;
 
-  const books = response.data;
-
-  return {
-    props: {
-      booksList: books,
-    },
-  };
+    return {
+      props: {
+        booksList: books,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    return {
+      props: {
+        booksList: { total: 0, totalPage: 0, books: [] },
+      },
+    };
+  }
 });
